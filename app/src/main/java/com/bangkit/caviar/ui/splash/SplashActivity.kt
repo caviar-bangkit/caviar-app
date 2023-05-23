1b0c3a77029731507d93dfc15a83fe304f976723
Tisna Hadiana <tisnahadiana@gmail.com>
2023-05-23 11:02:05 +0700

    Adding splash layout

---
+++ app/src/main/java/com/bangkit/caviar/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.caviar.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.caviar.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}

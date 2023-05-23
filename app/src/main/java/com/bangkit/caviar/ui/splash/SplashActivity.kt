e22a2c7266adf0227cb78eb6f554c8de066b6eff
Tisna Hadiana <tisnahadiana@gmail.com>
2023-05-23 11:07:19 +0700

    Adding logo for apps

--- app/src/main/java/com/bangkit/caviar/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bangkit/caviar/ui/splash/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.bangkit.caviar.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bangkit.caviar.MainActivity
 import com.bangkit.caviar.R
 
 class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DELAY_MS = 2000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DELAY_MS)
     }
 }
